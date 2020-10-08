732704d4e27b84311880cd95520c5a484b1aae46
Aditya Mandke <40426312+ekdnam@users.noreply.github.com>
2020-10-08 22:42:07 +0530

    Update keys.js

--- app/config/keys.js
+++ app/config/keys.js
@@ -1,4 +1,4 @@
-dbPassword = 'mongodb+srv://aditya123:<pict123>@cluster0.ekjy9.mongodb.net/<dbname>?retryWrites=true&w=majority'
+dbPassword = 'mongodb+srv://uname:<password>@cluster0.ekjy9.mongodb.net/<dbname>?retryWrites=true&w=majority'
 // mongodb+srv://aditya123:<password>@cluster0.ekjy9.mongodb.net/<dbname>?retryWrites=true&w=majority
 module.exports = {
     mongoURI: dbPassword
