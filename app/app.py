e9e9d1371905110ff998c8d70b372b2991cd7a93
ekdnam <adeetya.m@gmail.com>
2020-10-08 16:22:36 +0530

    add app, eda-2

---
+++ app/app.py
@@ -0,0 +1,17 @@
+import flask
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('index.html')
+
+@app.route('/description', methods=['GET', 'POST'])
+def get_data():
+    return('Hi')
+
+if __name__=='__main__':
+    app.jinja_env.auto_reload = True
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
+    app.run(debug=True)
